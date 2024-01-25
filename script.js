f34a3df6fc5e5190817d79409d4aaa1d6a3d07d5
Muskan Sheikh <144351188+mismuskan@users.noreply.github.com>
2024-01-25 20:12:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 const byteSize = (str) => {
   // write your code here
+if (str === '') {
+        return 0;
+    }
+
+    let byteCount = 0;
+
+    // Use TextEncoder to encode the string into bytes
+    const encoder = new TextEncoder();
+    const encodedBytes = encoder.encode(str);
+
+    // Count the number of bytes
+    for (let index = 0; index < encodedBytes.length; index++) {
+        byteCount++;
+    }
+
+    return byteCount;
+};
 };
 
 // Do not change the code below
-const str = prompt("Enter some string.");
-alert(byteSize(str));
+// const str = prompt("Enter some string.");
+// alert(byteSize(str));
