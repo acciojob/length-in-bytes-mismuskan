1bcc667beb3be7a0841a152af12397ff11b399f1
Muskan Sheikh <144351188+mismuskan@users.noreply.github.com>
2024-11-22 22:05:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
 const byteSize = (str) => {
   // write your code here
-	let count=0;
-	for(let i=0;i<str.length;i++){
-		count++;
-		return count;
-	}
-if (str === '') {
-        return 0;
-    }
-	console.log(count);
-
-    let byteCount = 0;
 
     // Use TextEncoder to encode the string into bytes
     const encoder = new TextEncoder();
